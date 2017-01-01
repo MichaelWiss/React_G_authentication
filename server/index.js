3b9282d16cf44b56a67640375ea82a212ae22aea
MichaelWiss <michael.wiss@gmail.com>
2017-01-01 13:26:54 -0600

    added nodemon

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const http = require('http');
-const bodyparser = require('body-parser');
+const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const app = express();
 
