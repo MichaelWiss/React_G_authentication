26128ffa57934162cff0268d102e4e56e6aebad1
MichaelWiss <michael.wiss@gmail.com>
2017-01-02 14:36:47 -0600

    bcrypt hash password

--- server/models/user.js
+++ server/models/user.js
@@ -8,6 +8,30 @@ const userSchema = new Schema({
      password: String
 });
 
+//on save hook, encrypt password
+// before saving a model, run this function
+userSchema.pre('save', function(next) {
+	//get access to the user model
+	const user = this;   //user email, password
+
+	//generate a salt then run callback
+
+	bcrypt.genSalt(10, function(err, salt) {
+        if (err) {return next(err); }
+
+        //hash(encrypt) our password using the salt
+
+		bcrypt.hash(user.password, salt, null, function(err, hash) {
+			if (err) { return next(err); }
+
+			//overwrite plain text password with encrypted password
+
+			user.password = hash;
+			next();
+		})
+	})
+})
+
 //create model class
 const ModelClass = mongoose.model('user', userSchema);
 
